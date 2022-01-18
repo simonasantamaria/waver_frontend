d840e385eacae9582b52594d3ecc6adc6058c229
simona.santamaria <simona.santamaria@hotmail.com>
2022-01-18 11:47:02 +0100

    database connected to mongodb remote

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,14 @@ import motor.motor_asyncio
 
 from model import Todo
 
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017/')
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+MONGODB_URI = config.get("MONGODB_URI")
+
+
+
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URI)
 database = client.TodoList
 collection = database.todo
 
