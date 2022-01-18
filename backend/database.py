b2b6289465bcd859d960539c6f6d7aabcb8a51cb
Simona Santamaria <52436556+simonasantamaria@users.noreply.github.com>
2022-01-18 10:36:50 +0100

    database_uri changed

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,7 @@ import motor.motor_asyncio
 
 from model import Todo
 
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017/')
+client = motor.motor_asyncio.AsyncIOMotorClient('mongodb+srv://simonalara:<password>@cluster0.aohks.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 database = client.TodoList
 collection = database.todo
 
